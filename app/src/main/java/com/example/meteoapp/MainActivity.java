5ac9814868ea2a33d95204939ce50149ddb1a445
antoine <antoine.lcts@gmail.com>
2020-12-06 14:30:18 +0100

    ajout du back end

--- app/src/main/java/com/example/meteoapp/MainActivity.java
+++ app/src/main/java/com/example/meteoapp/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.meteoapp;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    //Création d'objets graphique
+    ImageView imageViewIcone;
+    TextView textViewTemperature, textViewCondition;
+    EditText editText;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//Association des objets aux layout
+        imageViewIcone = findViewById(R.id.imageViewIcone);
+        textViewTemperature = findViewById(R.id.textViewTemperature);
+        textViewCondition = findViewById(R.id.textViewCondition);
+        editText = findViewById(R.id.editText);
+
+//Action du button
+        findViewById(R.id.button).setOnClickListener(v -> {
+            RequestQueue queue = Volley.newRequestQueue(this);
+
+            url = "https://www.prevision-meteo.ch/services/json/" + editText.getText().toString();
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    response -> {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            // current_condition
+                            JSONObject current_condition = jsonObject.getJSONObject("current_condition");
+                            String icone = current_condition.getString("icon_big");
+                            String tmp = current_condition.getString("tmp");
+                            String condition = current_condition.getString("condition");
+                            //Affichage des données météo
+                            Picasso.get().load(icone).into(imageViewIcone);
+                            textViewTemperature.setText("Temperature : " + tmp);
+                            textViewCondition.setText(condition);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(this, "Erreur, La ville n'a pas été trouvé", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                        //Messsage en cas d'erreur
+                    error -> Toast.makeText(this, "Erreur", Toast.LENGTH_SHORT).show());
+            queue.add(stringRequest);
+        });
     }
 }
